7c6d763d8188a73d7d5e3bc06bf88ce76e15e0f3
ashukla2 <ashukla2@IN-L2032.corp.yodlee.com>
2018-11-28 11:32:40 +0530

    ApplicationContext and property initialization

--- SpringDemo/src/org/kaushik/javabrains/DrawingApp.java
+++ SpringDemo/src/org/kaushik/javabrains/DrawingApp.java
@@ -1,14 +1,18 @@
 package org.kaushik.javabrains;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
 		//Triangle t =new Triangle();
-		ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml"); // XmlBeanFactory because here we will use xml configuration file 
+		// BeanFactory factory= new XmlBeanFactory(new FileSystemResource("spring.xml")); // XmlBeanFactory because here we will use xml configuration file 
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml"); // ApplicationContext can do everything that bean factory does, along with that it has some extra advantages and has same weight
 		Triangle triangle=(Triangle) context.getBean("triangle");
 		triangle.draw();
 		
--- SpringDemo/src/org/kaushik/javabrains/Triangle.java
+++ SpringDemo/src/org/kaushik/javabrains/Triangle.java
@@ -1,7 +1,19 @@
 package org.kaushik.javabrains;
 
 public class Triangle {
+	private String type;
+	public void setType(String type)
+	{
+		this.type=type;
+		
+	}
+	public String getType()
+	{
+		return type;
+	}
 	public void draw()
-	{System.out.println("Triangle drawn");}
+	{
+		System.out.println(getType() + " Triangle drawn");
+	}
 
 }
