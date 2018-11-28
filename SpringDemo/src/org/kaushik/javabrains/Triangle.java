b3041e721748fd22dbe7431a2962f7aa05bd63d7
ashukla2 <ashukla2@IN-L2032.corp.yodlee.com>
2018-11-28 12:11:47 +0530

    Using constructor injection

--- SpringDemo/src/org/kaushik/javabrains/Triangle.java
+++ SpringDemo/src/org/kaushik/javabrains/Triangle.java
@@ -1,19 +1,31 @@
 package org.kaushik.javabrains;
 
-public class Triangle {
+public class Triangle 
+{
 	private String type;
-	public void setType(String type)
-	{
-		this.type=type;
-		
-	}
+	private int height;
+
+    public Triangle(String type)
+    {
+	    this.type=type;
+    }
+
+    public Triangle(String type, int height) // 2 types of constructor are here. The one that matches with the format of xml is used
+    {
+    	this.type=type;
+    	this.height=height;
+    }
 	public String getType()
 	{
 		return type;
 	}
+	public int getHeight() 
+	{
+		return height;
+	}
 	public void draw()
 	{
-		System.out.println(getType() + " Triangle drawn");
+		System.out.println(getType() + " Triangle drawn of height " + getHeight());
 	}
 
 }
