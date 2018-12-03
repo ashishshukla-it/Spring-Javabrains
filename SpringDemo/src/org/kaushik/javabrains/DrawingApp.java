4437ff31ff356bdcfc1568a97fec7e20a8d2efd8
ashukla2 <ashukla2@IN-L2032.corp.yodlee.com>
2018-12-03 15:09:28 +0530

    Lifecycle callbacks 2

--- SpringDemo/src/org/kaushik/javabrains/DrawingApp.java
+++ SpringDemo/src/org/kaushik/javabrains/DrawingApp.java
@@ -13,14 +13,6 @@ public class DrawingApp {
 	public static void main(String[] args) {
 		AbstractApplicationContext context=new ClassPathXmlApplicationContext("spring.xml"); 
 	    context.registerShutdownHook();
-	    /* 
-	     * In spring, registerShutdownHook() method is used to shut down IoC container in non-web applications.
-	     * It shuts down IoC container gracefully. 
-	     * In non web based application like desk top application it is required to call registerShutdownHook. 
-	     * In our desktop application we need to release all resources used by our spring application. 
-	     * So we need to ensure that after application is finished, destroy method on our beans should be called. 
-	     * In web-based application ApplicationContext already implements code to shut down the IoC container properly. 
-	     * But in desktop application we need to call registerShutdownHook to shutdown IoC container properly.*/
 		Triangle triangle=(Triangle) context.getBean("triangle");
 		triangle.draw();
 		
@@ -31,9 +23,9 @@ public class DrawingApp {
 
 /* 
 Output: 
-InitializingBean init method called for triangle
+MyInit() method called for triangle
 PointA : ( 0 ,1)
 PointB : ( 20 ,21)
 PointC : ( 30 ,31)
-DisposableBean destroy method called for triangle
+MyDestroy() method called for triangle
 */
